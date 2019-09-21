16f51a0dcc82e30c33528d1345f7eb719afa5417
liujihong <51605419+ljhgithub1@users.noreply.github.com>
2019-09-21 21:27:03 +0800

    实现聊天

---
+++ websocket/src/main/java/com/ljh/websocket/configuration/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.ljh.websocket.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author Liu, jihong
+ * @version 1.0
+ * @date 2019-09-12 上午 10:54
+ */
+
+/**
+ * 将ServerEndpointExporter对象注入到spring容器中
+ * 这个bean会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
+ *
+ * @ServerEndpoint：就相当于requestmapping，接受客服端发过来的请求，进行映射
+ */
+@Configuration
+public class WebSocketConfig {
+
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
